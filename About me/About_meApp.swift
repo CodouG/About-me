5feb1a0fc40d1fb2f52c1c1196ba3bfa9821f81c
Codou Gueye <codoumbakhe@gmail.com>
2025-07-23 13:54:42 -0600

    Initial Commit

---
+++ About me/About_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_meApp.swift
+//  About me
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct About_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
